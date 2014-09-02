c68344e206ac13fd1553719fb5628b769464ddde
mac <403655356@qq.com>
2014-09-02 13:35:29 +0800

    Initial Commit

---
+++ CXYHBChess/CXYAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CXYAppDelegate.h
+//  CXYHBChess
+//
+//  Created by iMac on 14-9-2.
+//  Copyright (c) 2014年 ___cxy___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CXYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CXYHBChess/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CXYHBChess
+//
+//  Created by iMac on 14-9-2.
+//  Copyright (c) 2014年 ___cxy___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CXYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CXYAppDelegate class]));
+    }
+}
